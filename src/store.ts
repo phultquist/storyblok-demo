550bafb85797011f56ac7a3109fa6f8b2c49a6ed
Patrick Hultquist <patrick@monogram.io>
2022-09-09 13:46:19 +0200

    new store framework

--- src/store.ts
+++ src/store.ts
@@ -11,16 +11,18 @@ export const getStoryblokBridge = <T extends StoryblokComponentType<string> = an
 	initialValue: StoryData<T>,
 	options: StoryblokBridgeConfigV2
 ) => {
-	const store = writable<StoryData<T>>(initialValue);
+	const store = writable<StoryData<T>>(initialValue, (set) => {
+		// Use the useStoryblokBridge function to set the store value on change
+		const unsubscribe = useStoryblokBridge<T>(
+			id,
+			(newStory) => {
+				set(newStory);
+			},
+			options
+		);
 
-	// Use the useStoryblokBridge function to set the store value onChange
-	useStoryblokBridge<T>(
-		id,
-		(newStory) => {
-			store.set(newStory);
-		},
-		options
-	);
+		return unsubscribe;
+	});
 
 	// Return only the subscribe method, so it can't be written to
 	return { subscribe: store.subscribe };
